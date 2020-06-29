e35383ea3e6b471c1abe5daf5e8451577e9ed18a
Mariana Pereira <mariana.c.pereira.23@hotmail.com>
2020-06-29 13:53:59 +0000

    Added mouseenter and mouseleave effects to important links

--- static/js/myapp.js
+++ static/js/myapp.js
@@ -4,5 +4,11 @@ $(document).ready(function(){
     });
     $("#showpostform").click(function(){
         $("#postform").removeClass('hidden');
-    });  
+    });
+    $(".book_btn, .review_btn, .nav-link, .dropdown-item, .genre, .title").mouseenter(function(){
+        $(this).addClass('hovered');
+    });
+    $(".book_btn, .review_btn, .nav-link, .dropdown-item, .genre, .title").mouseleave(function(){
+        $(this).removeClass('hovered');
+    });
 });
