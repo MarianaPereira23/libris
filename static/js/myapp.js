997b640badc62836c31b673c9189b0cd7ab7ecba
Mariana Pereira <mariana.c.pereira.23@hotmail.com>
2020-06-30 13:26:01 +0000

    Finnished de map pop-up and made the rest of the contact page

--- static/js/myapp.js
+++ static/js/myapp.js
@@ -1,6 +1,6 @@
 //Map from contacts.html
 
-var mymap = L.map('map').setView([50.10, 14.41], 14);
+var mymap = L.map('map').setView([50.105, 14.41], 14);
 
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
@@ -17,8 +17,8 @@ if (width < 541) {
 
 //  MAP MARKERS - code written according to Leaflet documentation 
 
-var marker1 = L.marker([50.10, 14.41]).addTo(mymap)
-   .bindPopup('<p class="content-font">Address here</p>')
+var marker1 = L.marker([50.105, 14.41]).addTo(mymap)
+   .bindPopup('<p class="content-font">Fountain Stromovka, come meet us!</p>')
     .closePopup();
 
 
